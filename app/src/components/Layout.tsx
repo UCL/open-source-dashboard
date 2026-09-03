1266e12ca3496706bc7b2a6942cf662c77d8729a
Sam Cunliffe <s.cunliffe@ucl.ac.uk>
2026-09-03 21:12:29 +0100

    Revert to using the adaptive SVG.

--- app/src/components/Layout.tsx
+++ app/src/components/Layout.tsx
@@ -21,8 +21,8 @@ export const Layout: FC<PropsWithChildren> = ({ children }) => {
       <div className="flex flex-row items-center justify-between">
         <div className="flex flex-row items-center gap-6">
           <Image
-            className="block h-8 w-auto dark:invert"
-            src={`${basePath}/images/ucl-logo-black.svg`}
+            className="block h-8 w-auto"
+            src={`${basePath}/images/ucl-dark-light-mode-adaptive.svg`}
             height={50}
             width={150}
             alt="UCL logo"
